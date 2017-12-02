fca3dd536354226d633f700735f57ef498ae46ae
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-12-02 10:53:42 -0500

    Initial Commit

---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+	// private properties
+	private Animator animator;
+	private bool _isFacingRight = true;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis ("Horizontal");
+		float jump = Input.GetAxis ("Jump");
+
+		// if the right button is pressed (either d or right arrow)
+		if (move > 0.3f) {
+			animator.SetInteger ("AnimState", 1);
+			_isFacingRight = true;
+		}
+		else if (move < -0.3f) {
+				animator.SetInteger ("AnimState", 1);
+			_isFacingRight = false;
+		} else {
+			animator.SetInteger ("AnimState", 0);
+		}
+			
+
+		if (jump > 0.3f) {
+			animator.SetInteger ("AnimState", 2);
+		}
+
+		gameObject.transform.localScale = (!_isFacingRight) 
+			? new Vector3 (-1f, 1f, 1f) : new Vector3 (1f, 1f, 1f);
+	}
+}
